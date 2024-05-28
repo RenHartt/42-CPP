3650d46f2e769dd380604be98cd4a7cf26ad3db8
Baptiste Goron <bgoron@3G4.42angouleme.fr>
2024-05-28 16:09:56 +0200

    remove useless close()

--- CPP 01/ex04/replace.cpp
+++ CPP 01/ex04/replace.cpp
@@ -6,7 +6,7 @@
 /*   By: bgoron <bgoron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 17:29:39 by bgoron            #+#    #+#             */
-/*   Updated: 2024/05/19 20:44:53 by bgoron           ###   ########.fr       */
+/*   Updated: 2024/05/28 16:08:55 by bgoron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,17 @@
 int	printError(std::string error)
 {
 	std::cout << "Error: " << error << std::endl;
+
 	return (1);
 }
 
 int	openFile(std::string fileName, std::ifstream &oldFile, std::ofstream &newFile)
 {
 	oldFile.open(fileName.c_str(), std::ios::in);
-	if (!oldFile.is_open())
+	if (!oldFile.is_open() || !newFile.is_open())
 		return (printError("could not open file"));
 	newFile.open((fileName + ".replace").c_str(), std::ios::out);
-	if (!newFile.is_open())
-	{
-		oldFile.close();
-		return (printError("could not create file"));
-	}
+
 	return (0);
 }
 
@@ -40,7 +37,7 @@ int	replace(std::string fileName, std::string s1, std::string s2)
 	std::string			line;
 	size_t				pos = 0;
 
-	if (fileName.empty() || s1.empty() || s2.empty())
+	if (fileName.empty() || s1.empty())
 		return (printError("empty string"));
 	if (openFile(fileName, oldFile, newFile))
 		return (1);
@@ -53,7 +50,6 @@ int	replace(std::string fileName, std::string s1, std::string s2)
 		pos += s2.length();
 	}
 	newFile << line;
-	oldFile.close();
-	newFile.close();
+
 	return (0);
 }
